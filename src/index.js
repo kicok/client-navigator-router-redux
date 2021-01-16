74e9c5b22f355fff2a8f70449ac25807642038ea
kicok <kicoker@gmail.com>
2021-01-16 21:48:35 +0900

    Using Redux DevTools to inspect the Store & Debuging Session

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 
 import App from './components/App';
 
-const store = createStore(reducers);
+// https://github.com/zalmoxisus/redux-devtools-extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// http://localhost:3000/?debug_session=some_string
+//새로고침을 해도 DevTools에서 상태값이 사라지지 않고 그대로 유지됨
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 ReactDOM.render(
   <Provider store={store}>
     <App />
