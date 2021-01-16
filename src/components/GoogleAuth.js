fc1f8cfa31e5c45112d85866dec1203906f7b921
kicok <kicoker@gmail.com>
2021-01-16 17:14:06 +0900

    state -> props

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null }; // 로그인 여부를 알수 없으므로 null로 초기화
   componentDidMount() {
     // gapi는 브라우저 쪽 자바스크립트를 위한 구글 클라이언트 라이브러리
     // 구글 로그인, 드라이브등 api를 쉽게 연결할수 있도록 도와줌.
@@ -19,8 +18,10 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
 
+          //this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          // 위의 코드를 주석처리하고 아래의 코드로 변경 : state가 아닌 mapStateToProps 에서 전달받은 props로 처리하기 위해..
+          this.onAuthChange(this.auth.isSignedIn.get());
           // chrome의 콘솔창에서 gapi.auth2.getAuthInstance().isSignedIn 을 입력하면 __proto__ 내에 listen: ƒ (a) 을 통해서 들을수 있다.
           this.auth.isSignedIn.listen(this.onAuthChange);
 
@@ -45,9 +46,9 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon"></i>
@@ -68,4 +69,9 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = (state) => {
+  console.log(state);
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
