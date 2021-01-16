a91631ccb8abd7d9f084af8d22a852b969ed09fd
kicok <kicoker@gmail.com>
2021-01-16 18:40:53 +0900

    Recording the user Id

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -33,7 +33,7 @@ class GoogleAuth extends React.Component {
   onAuthChange = (isSignedIn) => {
     // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     // Action Creators 호출
-    if (isSignedIn) this.props.signIn();
+    if (isSignedIn) this.props.signIn(this.auth.currentUser.get().getId());
     else this.props.signOut();
   };
 
@@ -70,7 +70,6 @@ class GoogleAuth extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
   return { isSignedIn: state.auth.isSignedIn };
 };
 
