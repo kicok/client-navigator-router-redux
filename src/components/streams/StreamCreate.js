4482d273ceda88b3cf9b7cfc7587cd0a310a2209
kicok <kicoker@gmail.com>
2021-01-20 14:55:10 +0900

    11.Handling-Form-Submission

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -24,15 +24,25 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValue) {
+    // 더이상 event.preventDefault()를 사용할 필요가 없다.
+    console.log(formValue);
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <form className="ui form">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
           component={this.renderInput}
           label="Enter Description"
         />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
