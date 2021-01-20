3a431942e9e74405d54d6ba907dd0f00c7a4c8f2
kicok <kicoker@gmail.com>
2021-01-20 10:38:38 +0900

    09.Creating-Form_And_Auto-Handling-Events

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+//class기반의 컴포턴트에 도우미 메서드가 많아서 코드를 잘 구성할수 있기 때문에 class 로 변경함
+class StreamCreate extends React.Component {
+  // renderInput(formProps) {
+  //   console.log(formProps);
+  //   return (
+  //     <input
+  //       onChange={formProps.input.onChange}
+  //       value={formProps.input.value}
+  //     />
+  //   );
+  // }
 
-export default StreamCreate;
+  // 위와 같은 방식으로 속성을 하나하나 추가하지 않고
+  // 아래의 방식으로하면 모든 속성을 한번에 꺼내서 추가한다.
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
