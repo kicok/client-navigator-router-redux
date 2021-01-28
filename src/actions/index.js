97104bbd346d8faafa0d9b2ce50e7f9136c3c713
kicok <kicoker@gmail.com>
2021-01-28 17:22:06 +0900

    Associating Streams with Users

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId }); //객체에 userId를 추가.
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
