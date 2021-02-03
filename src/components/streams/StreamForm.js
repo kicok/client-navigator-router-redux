38a83647190c3906cfd4414afd46f804f2c54bfd
kicok <kicoker@gmail.com>
2021-02-03 17:46:16 +0900

    Real-Code-ReUse
    Correct StreamForm

---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+// StreamCreate.js. StreamEdit.js 에서 사용되는  form 의 재사용을 위하여 활용
+class StreamForm extends React.Component {
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} placeholder={label} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  onSubmit = (formValue) => {
+    this.props.onSubmit(formValue);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate: validate,
+})(StreamForm);
