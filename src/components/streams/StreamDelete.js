3ec4aa307d7c6e63f315052dbc6b1273409a6e1d
kicok <kicoker@gmail.com>
2021-02-17 18:37:01 +0900

    Delete Stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
-    console.log(this.props);
-    this.props.fetchStream(this.props.match.params.id);
+    this.props.fetchStream(this.props.match.params.id); // state 에 stream 생성(새로고침하거나 주소로 바로 들어올때 필요)
   }
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
+        {this.props.stream && (
+          <button
+            onClick={() => this.props.deleteStream(id)}
+            className="ui button negative"
+          >
+            Delete
+          </button>
+        )}
+        <Link to="/" className="ui button">
+          {this.props.stream ? 'Cancel' : 'Ok'}
+        </Link>
       </>
     );
   }
 
   renderContent() {
     if (!this.props.stream) {
-      return 'Are you sure you want to delete this stream?';
+      return 'No Stream for Delete';
     }
 
     return `Are you sure you want to delete this stream with title : ${this.props.stream.title}? `;
@@ -45,4 +55,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
