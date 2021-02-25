e473b5f7126502c726d67e641d3235037f02c232
kicok <kicoker@gmail.com>
2021-02-25 18:20:55 +0900

    Creating a FLV Player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -13,6 +18,9 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
+        {/* controls 는 default 값이 true이므로 아래의 코드와 같은 코드임
+        <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />  */}
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
